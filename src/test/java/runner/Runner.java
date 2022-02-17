e12624aec0861629c55a88b63e66a580d96ab622
Sravanthi-Harish <sravanthisankuratri@gmail.com>
2022-02-17 14:09:47 +0530

    Run Class changes

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,22 +1,19 @@
 package runner;
 
-import java.io.File;
-
-import org.junit.AfterClass;
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "stepDef", plugin = { "pretty",
 		"html:target/cucumber-reports.html", "json:target/cucumber.json",
 		}, tags = "@swaglab")
-
-public class TestRunner1 {
+public class Runner {
 	public static void sendEmail() {
 
 		System.out.println("Send Email of execution");
 		
 	}
+
 }
